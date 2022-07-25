174ff9a558cc229ba4b83fb8bd01d8b5423e8a63
a-klimovich <ar2r.klimovich@gmail.com>
2022-07-25 20:48:08 +0200

    base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import DocIcon from './assets/image/files-icon/doc.svg';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         header
+        <DocIcon />
+        {/* EmptyFileIcon */}
       </header>
     </div>
   );
---
+++ src/components/UI/icons/Files.js
@@ -0,0 +1,14 @@
+// import DocIcon from '../../../assets/image/files-icon/doc.svg';
+// import EmptyFileIcon from './empty-file.svg';
+// import FileIcon from './file.svg';
+// import JpgIcon from './jpg.svg';
+// import MusikIcon from './musik.svg';
+// import ZipIcon from './zip.svg';
+// import XlsIcon from './XLS.svg';
+// import VideoIcon from './video.svg';
+// import TxtIcon from './txt.svg';
+// import RarIcon from './rar.svg';
+// import PointIcon from './point.svg';
+// import PdfIcon from './pdf.svg';
+
+
