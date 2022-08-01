58dba45ef37d8ae85e6c6e154126d28ae32f3800
artur.klimovich <artur.klimovich@silksh.pl>
2022-08-01 19:26:08 +0200

    sorter table

--- src/components/fileTable/__common/base/FileTableColumn.js
+++ src/components/fileTable/__common/base/FileTableColumn.js
@@ -6,10 +6,18 @@ const columns = [
   {
     title: 'Рекомендации',
     dataIndex: 'recommendations',
+    sorter: {
+      compare: (x, y) => {return (x.recommendations === y.recommendations) ? 0 : x.recommendations? -1 : 1},
+      multiple: 2,
+    },
   },
   {
     title: 'Избранное',
     dataIndex: 'favorite',
+    sorter: {
+      compare: (x, y) => {return (x.favorite === y.favorite) ? 0 : x.favorite? -1 : 1},
+      multiple: 1,
+    },
   },
   {
     title: 'Последнее изменение',
