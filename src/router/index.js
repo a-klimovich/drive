8fb5170fb2a5450973fb06aff7866f1b448a8c2d
a-klimovich <ar2r.klimovich@gmail.com>
2022-09-03 16:16:25 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Folders from "../../components/Folders";
+import Folders from "../components/Folders";
 
 const Router = () => {
   return (
